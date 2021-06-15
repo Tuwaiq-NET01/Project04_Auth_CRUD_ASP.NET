76e44392228a25e19fef919da20782a3ea6b1d01
1Riyad <riyad.alghamdi@tuwaiq.edu.sa>
2021-06-15 23:13:12 +0300

    v0.7.1: Reimplement the Edit action for a security best practice to prevent overposting when updating a record of Products. And Add MyHub action

--- Keraa/Keraa/Controllers/ProductsController.cs
+++ Keraa/Keraa/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using Keraa.Models;
 using Microsoft.AspNetCore.Identity; // for:_userManager
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -80,20 +81,49 @@ namespace Keraa.Controllers
 
         //POST - /products/edit/id
         [HttpPost]
-        public IActionResult Edit([Bind("Id", "Name", "ShortDesc", "CoverImage")] ProductModel prod)
+        public async Task<IActionResult> Edit([Bind("Id", "Name", "ShortDesc", "CoverImage")] ProductModel prod)
         {
-            //var Product = _db.Products.ToList().Find(p => p.Id == id);
-            //Product.Name = prod.Name;
-            //Product.Description = prod.Description;
-            //Product.Price = prod.Price;
+            /*
+             --  This implementation is a security best practice to prevent overposting when updating a record of Products.
+             --  TryUpdateModel used to update fields in the retrieved entity based on user input in the posted form data.
+             -- As a best practice to prevent overposting, the fields that you want to be updateable by the Edit page
+             are declared in the TryUpdateModel parameters. (The empty string preceding the list of fields in
+             the parameter list is for a prefix to use with the form fields names.) Currently there are no 
+             extra fields that you're protecting, but listing the fields that you want the model binder to bind
+             ensures that if you add fields to the data model in the future, they're automatically protected
+             until you explicitly add them here. 
+            Resource: https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/crud?view=aspnetcore-5.0#security-note-about-overposting
+             */
 
-            //_db.Products.Update(Product);
-            //_db.SaveChanges();
-            _db.Products.Update(prod);
+            var productToUpdate = await _db.Products.FirstOrDefaultAsync(s => s.Id == prod.Id);
+            if (await TryUpdateModelAsync<ProductModel>(
+                productToUpdate,
+                "",
+                s => s.Name, s => s.ShortDesc, s => s.Catagory, s => s.CoverImage))
+            {
+                try
+                {
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException /* ex */)
+                {
+                    //Log the error (uncomment ex variable name and write a log.)
+                    ModelState.AddModelError("", "Unable to save changes. " +
+                        "Try again, and if the problem persists, " +
+                        "see your system administrator. And can Find me at GitHub: @1Riyad");
+                }
+            }
+
+
+/*
+        public async Task<IActionResult> Edit([Bind("Id", "Name", "ShortDesc", "CoverImage")] ProductModel prod)
+        {*/
+                _db.Products.Update(prod);
             _db.SaveChanges();
-
             return RedirectToAction("Index");
         }
+
 
         // POST - /products/delete/id
         [HttpPost]
@@ -108,6 +138,25 @@ namespace Keraa.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+
+
+        // GET: /products/id
+        public async Task<IActionResult> MyHub()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var Product = _db.Products.Where(product => product.OwnerId == user.Id).ToList();
+            if (user.Id == null || Product == null)
+            {
+                return View("_NotFound");
+            }
+            ViewData["products"] = Product;
+            return View("Index", Product);
+
+        }
+
+
 
     }
 }
