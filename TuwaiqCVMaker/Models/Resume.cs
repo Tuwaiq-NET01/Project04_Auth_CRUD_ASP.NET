1d778337c6f180caa823a429a07abf0f1447e5f6
Ahmed Alzubaidi <Ahmed.Alzubaidi@tuwaiq.edu.sa>
2021-06-16 15:32:32 +0300

    feat: added Copy method to Resume

--- TuwaiqCVMaker/Models/Resume.cs
+++ TuwaiqCVMaker/Models/Resume.cs
@@ -15,5 +15,16 @@ namespace TuwaiqCVMaker.Models
         [Required] public string UserId { get; set; }
         [JsonIgnore] public ApplicationUser User { get; set; }
         public ICollection<Skill> Skills { get; set; }
+
+        public void Copy(Resume resume)
+        {
+            this.Name = resume.Name;
+            this.Email = resume.Email;
+            this.Phone = resume.Phone;
+            this.Introduction = resume.Introduction;
+            this.Education = resume.Education;
+            this.UserId = resume.UserId;
+            this.Skills = resume.Skills;
+        }
     }
 }
