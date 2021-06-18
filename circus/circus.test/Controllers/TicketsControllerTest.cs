64556e50a6ba94946b6a79d40c1c48b4ff930db0
Hanan <82478654+hanan-tuwaiq@users.noreply.github.com>
2021-06-18 21:55:54 +0300

    project complete

--- circus/circus.test/Controllers/TicketsControllerTest.cs
+++ circus/circus.test/Controllers/TicketsControllerTest.cs
@@ -1,13 +1,16 @@
 ﻿using circus.Controllers;
 using circus.Data;
 using circus.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -44,32 +47,35 @@ namespace circus.test.Controllers
                 new TicketModel(){Id= 1, Quantity= 2, ShowId = 1, UserId = "1"},
                 new TicketModel(){Id= 2, Quantity= 4, ShowId = 2, UserId = "2"}
             };
-            var users = new[]
-            {
-                new IdentityUser(){Id = "1"},
-                new IdentityUser(){Id="2"}
-            };
             context.Shows.AddRange(shows);
-            context.Users.AddRange(users);
             context.Performers.AddRange(performers);
             context.Tickets.AddRange(tickets);
             context.SaveChanges();
         }
         [Test]
-        public void indexTest_NullPageView()
+        public void indexTest_UserIsAuthenticated()
         {
-            ViewResult result = tc.Index() as ViewResult;
-            Assert.IsNull(result.ViewName);
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {
+                                        new Claim(ClaimTypes.NameIdentifier, "Hello"),
+                                        new Claim(ClaimTypes.Name, "hi@example.com")
+                                        // other required and custom claims
+                                   }, "TestAuthentication"));
+            var controller = tc;
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = new DefaultHttpContext { User = user };
+            var result = controller.Index() as ViewResult;
+            var viewName = result.ViewName;
+            Assert.True(string.IsNullOrEmpty(viewName) || viewName == "Index");
         }
         [Test]
-        public void indexTest_NotNullPageView()
+        public void indexTest_RedirectToUnauthenticatedUser()
         {
-            IdentityUser user = new IdentityUser();
-           
-            var ticket = tc.Index();
-            ViewResult result = ticket as ViewResult;
-           // Assert.IsTrue(user.Id == "1");
+            var controller = tc;
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = new DefaultHttpContext { User = null };
+            var result = tc.Index() as RedirectResult;
+            Assert.That(result.Url.Equals("~/Identity/Account/Login"));
+        
         }
-     
     }
 }
