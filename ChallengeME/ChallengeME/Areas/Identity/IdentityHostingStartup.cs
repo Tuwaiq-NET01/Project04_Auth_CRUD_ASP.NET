b1ce7abd0c5c8cf1aa26915cabecff002e409739
Mohammed Rashed <Mohammed.Albalawi@tuwaiq.edu.sa>
2021-06-18 21:28:21 +0300

    Login and Register styling

---
+++ ChallengeME/ChallengeME/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ChallengeME.Data;
+using ChallengeME.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ChallengeME.Areas.Identity.IdentityHostingStartup))]
+namespace ChallengeME.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
