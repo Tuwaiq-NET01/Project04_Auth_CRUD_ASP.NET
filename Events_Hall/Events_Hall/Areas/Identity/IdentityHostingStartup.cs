86b705816b2966475f8c0353c46da9be4452a407
Reema3Alyousef <reema.alyousef@tuwaiq.edu.sa>
2021-06-18 03:51:01 +0300

    made it slightly prettier

---
+++ Events_Hall/Events_Hall/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Events_Hall.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Events_Hall.Areas.Identity.IdentityHostingStartup))]
+namespace Events_Hall.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
