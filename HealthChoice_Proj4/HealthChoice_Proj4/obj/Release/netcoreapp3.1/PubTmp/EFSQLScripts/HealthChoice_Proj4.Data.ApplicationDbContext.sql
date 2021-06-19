18da14cf4234780f57d048ca116e724c4479ad43
Samirah.Alhusayni <Samirah.alhusayni@tuwaiq.edu.sa>
2021-06-19 03:59:45 +0300

    Final Version

---
+++ HealthChoice_Proj4/HealthChoice_Proj4/obj/Release/netcoreapp3.1/PubTmp/EFSQLScripts/HealthChoice_Proj4.Data.ApplicationDbContext.sql
@@ -0,0 +1,396 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE TABLE [AspNetRoles] (
+        [Id] nvarchar(450) NOT NULL,
+        [Name] nvarchar(256) NULL,
+        [NormalizedName] nvarchar(256) NULL,
+        [ConcurrencyStamp] nvarchar(max) NULL,
+        CONSTRAINT [PK_AspNetRoles] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE TABLE [AspNetUsers] (
+        [Id] nvarchar(450) NOT NULL,
+        [UserName] nvarchar(256) NULL,
+        [NormalizedUserName] nvarchar(256) NULL,
+        [Email] nvarchar(256) NULL,
+        [NormalizedEmail] nvarchar(256) NULL,
+        [EmailConfirmed] bit NOT NULL,
+        [PasswordHash] nvarchar(max) NULL,
+        [SecurityStamp] nvarchar(max) NULL,
+        [ConcurrencyStamp] nvarchar(max) NULL,
+        [PhoneNumber] nvarchar(max) NULL,
+        [PhoneNumberConfirmed] bit NOT NULL,
+        [TwoFactorEnabled] bit NOT NULL,
+        [LockoutEnd] datetimeoffset NULL,
+        [LockoutEnabled] bit NOT NULL,
+        [AccessFailedCount] int NOT NULL,
+        CONSTRAINT [PK_AspNetUsers] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE TABLE [AspNetRoleClaims] (
+        [Id] int NOT NULL IDENTITY,
+        [RoleId] nvarchar(450) NOT NULL,
+        [ClaimType] nvarchar(max) NULL,
+        [ClaimValue] nvarchar(max) NULL,
+        CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE TABLE [AspNetUserClaims] (
+        [Id] int NOT NULL IDENTITY,
+        [UserId] nvarchar(450) NOT NULL,
+        [ClaimType] nvarchar(max) NULL,
+        [ClaimValue] nvarchar(max) NULL,
+        CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE TABLE [AspNetUserLogins] (
+        [LoginProvider] nvarchar(128) NOT NULL,
+        [ProviderKey] nvarchar(128) NOT NULL,
+        [ProviderDisplayName] nvarchar(max) NULL,
+        [UserId] nvarchar(450) NOT NULL,
+        CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY ([LoginProvider], [ProviderKey]),
+        CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE TABLE [AspNetUserRoles] (
+        [UserId] nvarchar(450) NOT NULL,
+        [RoleId] nvarchar(450) NOT NULL,
+        CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY ([UserId], [RoleId]),
+        CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE TABLE [AspNetUserTokens] (
+        [UserId] nvarchar(450) NOT NULL,
+        [LoginProvider] nvarchar(128) NOT NULL,
+        [Name] nvarchar(128) NOT NULL,
+        [Value] nvarchar(max) NULL,
+        CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY ([UserId], [LoginProvider], [Name]),
+        CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE INDEX [IX_AspNetRoleClaims_RoleId] ON [AspNetRoleClaims] ([RoleId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE UNIQUE INDEX [RoleNameIndex] ON [AspNetRoles] ([NormalizedName]) WHERE [NormalizedName] IS NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE INDEX [IX_AspNetUserClaims_UserId] ON [AspNetUserClaims] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE INDEX [IX_AspNetUserLogins_UserId] ON [AspNetUserLogins] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE INDEX [IX_AspNetUserRoles_RoleId] ON [AspNetUserRoles] ([RoleId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE INDEX [EmailIndex] ON [AspNetUsers] ([NormalizedEmail]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE UNIQUE INDEX [UserNameIndex] ON [AspNetUsers] ([NormalizedUserName]) WHERE [NormalizedUserName] IS NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'00000000000000_CreateIdentitySchema', N'3.1.15');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616111701_DBnameEdit')
+BEGIN
+    ALTER TABLE [AspNetUsers] ADD [FullName] nvarchar(max) NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616111701_DBnameEdit')
+BEGIN
+    ALTER TABLE [AspNetUsers] ADD [Discriminator] nvarchar(max) NOT NULL DEFAULT N'';
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616111701_DBnameEdit')
+BEGIN
+    CREATE TABLE [Events] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [Date] datetime2 NOT NULL,
+        [Overview] nvarchar(max) NULL,
+        [Image] nvarchar(max) NULL,
+        [Location] nvarchar(max) NULL,
+        CONSTRAINT [PK_Events] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616111701_DBnameEdit')
+BEGIN
+    CREATE TABLE [MembberShips] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NULL,
+        [Benefits] nvarchar(max) NULL,
+        CONSTRAINT [PK_MembberShips] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616111701_DBnameEdit')
+BEGIN
+    CREATE TABLE [Services] (
+        [Id] int NOT NULL IDENTITY,
+        [servName] nvarchar(max) NULL,
+        [servOverview] nvarchar(max) NULL,
+        [servLogo] nvarchar(max) NULL,
+        [servWebsite] nvarchar(max) NULL,
+        [servLocation] nvarchar(max) NULL,
+        [servType] nvarchar(max) NULL,
+        CONSTRAINT [PK_Services] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616111701_DBnameEdit')
+BEGIN
+    CREATE TABLE [Favorites] (
+        [Id] int NOT NULL IDENTITY,
+        [UserId] nvarchar(450) NULL,
+        [EventId] int NOT NULL,
+        CONSTRAINT [PK_Favorites] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Favorites_Events_EventId] FOREIGN KEY ([EventId]) REFERENCES [Events] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_Favorites_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE NO ACTION
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616111701_DBnameEdit')
+BEGIN
+    CREATE TABLE [Comments] (
+        [Id] int NOT NULL IDENTITY,
+        [Content] nvarchar(max) NOT NULL,
+        [Date] datetime2 NOT NULL,
+        [ServiceId] int NOT NULL,
+        CONSTRAINT [PK_Comments] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Comments_Services_ServiceId] FOREIGN KEY ([ServiceId]) REFERENCES [Services] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616111701_DBnameEdit')
+BEGIN
+    CREATE INDEX [IX_Comments_ServiceId] ON [Comments] ([ServiceId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616111701_DBnameEdit')
+BEGIN
+    CREATE INDEX [IX_Favorites_EventId] ON [Favorites] ([EventId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616111701_DBnameEdit')
+BEGIN
+    CREATE INDEX [IX_Favorites_UserId] ON [Favorites] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616111701_DBnameEdit')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210616111701_DBnameEdit', N'3.1.15');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616115233_EventSeeding')
+BEGIN
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Date', N'Image', N'Location', N'Name', N'Overview') AND [object_id] = OBJECT_ID(N'[Events]'))
+        SET IDENTITY_INSERT [Events] ON;
+    INSERT INTO [Events] ([Id], [Date], [Image], [Location], [Name], [Overview])
+    VALUES (1, '2020-06-01T00:00:00.0000000', N'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW3B-BS2WusiV3saTLR6geS3arZRqonNxsBl4PrmunO34ndoXNNq_feMD3_ir62-6My-A&usqp=CAU', N'Madinah', N'Ezz Events', N'Creative apps');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Date', N'Image', N'Location', N'Name', N'Overview') AND [object_id] = OBJECT_ID(N'[Events]'))
+        SET IDENTITY_INSERT [Events] OFF;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616115233_EventSeeding')
+BEGIN
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Date', N'Image', N'Location', N'Name', N'Overview') AND [object_id] = OBJECT_ID(N'[Events]'))
+        SET IDENTITY_INSERT [Events] ON;
+    INSERT INTO [Events] ([Id], [Date], [Image], [Location], [Name], [Overview])
+    VALUES (2, '2020-04-09T00:00:00.0000000', N'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw9nRLwpIt897OeK8n3DCj7l1VCFEGtf2p8jU2fE0wgYLmfQ60KlY1SVYbMDwq96vv6tM&usqp=CAU', N'Raif', N'Samirah Events', N'Creative apps');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Date', N'Image', N'Location', N'Name', N'Overview') AND [object_id] = OBJECT_ID(N'[Events]'))
+        SET IDENTITY_INSERT [Events] OFF;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616115233_EventSeeding')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210616115233_EventSeeding', N'3.1.15');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616140129_AddRelationUsersMemberShips')
+BEGIN
+    ALTER TABLE [AspNetUsers] ADD [MembershipID] int NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616140129_AddRelationUsersMemberShips')
+BEGIN
+    ALTER TABLE [AspNetUsers] ADD [MembershipsModelId] int NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616140129_AddRelationUsersMemberShips')
+BEGIN
+    CREATE INDEX [IX_AspNetUsers_MembershipsModelId] ON [AspNetUsers] ([MembershipsModelId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616140129_AddRelationUsersMemberShips')
+BEGIN
+    ALTER TABLE [AspNetUsers] ADD CONSTRAINT [FK_AspNetUsers_MembberShips_MembershipsModelId] FOREIGN KEY ([MembershipsModelId]) REFERENCES [MembberShips] ([Id]) ON DELETE NO ACTION;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210616140129_AddRelationUsersMemberShips')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210616140129_AddRelationUsersMemberShips', N'3.1.15');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210618210412_EventsUpdationgData')
+BEGIN
+    UPDATE [Events] SET [Date] = '2021-06-01T00:00:00.0000000', [Image] = N'https://cdn.expatwoman.com/s3fs-public/630601bc-81d3-450a-aabf-c7b68907c1e1.jpg', [Location] = N'King Fahd International Stadium in Riyadh', [Name] = N' Race of Champions motorsport event', [Overview] = N'The motorsport event will be held in the King Fahd International Stadium in Riyadh and is causing great excitement throughout the Kingdom. On the official website of Race of Champions it was announced that this is going to be a family event with activities for everyone.The official statement read: Further details, driver announcements and exact dates for ROC''s Saudi Arabian debut will be announced shortly, but the General Sports Authority has confirmed that Race Of Champions will be a family event, open to both genders, and feature drivers, exhibitions and activities all suitable for the Kingdom''s specific cultural requirements.'
+    WHERE [Id] = 1;
+    SELECT @@ROWCOUNT;
+
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210618210412_EventsUpdationgData')
+BEGIN
+    UPDATE [Events] SET [Image] = N'https://cdn.expatwoman.com/s3fs-public/editorial/iStock-623824284.jpg', [Location] = N'King Faisal International Stadium in Riyadh', [Name] = N'World Boxing Supreme Series Cruiserweight final', [Overview] = N'Jeddah will be hosting the World Boxing Supreme Series Cruiserweight final in May and spectators will be able to see the two best fighters in the cruiserweight division battle for the title. Local fighters will also have their chance to show off their skills in the ring as a part of the event.'
+    WHERE [Id] = 2;
+    SELECT @@ROWCOUNT;
+
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210618210412_EventsUpdationgData')
+BEGIN
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Date', N'Image', N'Location', N'Name', N'Overview') AND [object_id] = OBJECT_ID(N'[Events]'))
+        SET IDENTITY_INSERT [Events] ON;
+    INSERT INTO [Events] ([Id], [Date], [Image], [Location], [Name], [Overview])
+    VALUES (3, '2020-03-23T00:00:00.0000000', N'https://www.arabnews.com/sites/default/files/styles/n_670_395/public/2019/10/26/1817071-205482815.jpg?itok=TC967vOy', N'Saudi Arabia’s capital city Riyadh', N'Riyadh runners enjoy the ‘happiest 5k on the planet’', N'Held in Saudi Arabia’s capital city Riyadh between March 23 and April 1, The Saudi Games will gather 6,000 female and male athletes expected to participate in 40 sports, including swimming, basketball, athletics, archery. The highest gold medalist prize is set at one million riyals (around USD 266,500), while the second place will receive 300,000 riyals and third place 100,000 riyals. The event will also have some 2,000 technical and administrative supervisors from 13 provinces in the Kingdom.');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Date', N'Image', N'Location', N'Name', N'Overview') AND [object_id] = OBJECT_ID(N'[Events]'))
+        SET IDENTITY_INSERT [Events] OFF;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210618210412_EventsUpdationgData')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210618210412_EventsUpdationgData', N'3.1.15');
+END;
+
+GO
+
