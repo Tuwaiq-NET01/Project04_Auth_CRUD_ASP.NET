e60834adc69e03a53849b23abf4e2c6f2fc38a6e
يونس التركي <younes.alturkey@tuwaiq.edu.sa>
2021-06-28 11:39:44 +0300

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -42,7 +42,7 @@ export default function Footer() {
         <Grid item lg={3} md={12} xs={12} align="center">
           <ButtonBase
             className={classes.button}
-            href="https://bitchunkapi.herokuapp.com/swagger/index.html"
+            href="https://bitchunk-api.azurewebsites.net/swagger/index.html"
             target="_blank"
           >
             <Typography align="center">Bitchunk API</Typography>
